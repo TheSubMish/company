6f71ad2e4b4abd44ca70579c71e2dcf6e17e251e
TheSubMish <100989223+TheSubMish@users.noreply.github.com>
2022-10-09 20:50:47 +0545

    Create script.js

--- js/script.js
+++ js/script.js
@@ -1,118 +1,89 @@
 //Sign-Up Validation
-function signValid(){
-    let fname = document.getElementById('fname').value;
-    let lname = document.getElementById('lname').value;
-    let username =document.getElementById('username').value;
-    let email = document.getElementById('eamil').value;
-    let npassword = document.getElementById('npassword').value;
-    let cpassword = document.getElementById('cpassword').value;
+var fname = document.getElementById('fname').value;
+var lname = document.getElementById('lname').value;
+var user = document.getElementById('username').value;
+var email = document.getElementById('eamil').value;
+var npassword = document.getElementById('npassword').value;
+var cpassword = document.getElementById('cpassword').value;
 
-    if(fname=='' || fname == null && lname=='' || lname == null && username=='' || username == null && email=='' || email == null && npassword=='' || npassword == null && cpassword=='' || cpassword == null){
+function signValid() {
+
+    if ((fname == '' || fname == null) && (lname == '' || lname == null) && (user == '' || user == null) && (email == '' || email == null) && (npassword == '' || npassword == null) && (cpassword == '' || cpassword == null)) {
         alert('All fields are required');
         return false;
-    }
-    if(fname=='' || fname==null){
+    } else if (fname == '' || fname == null) {
         alert('First Name field is required');
         return false;
-    }
-    if(lname=='' || lname==null){
+    } else if (lname == '' || lname == null) {
         alert('Last Name field is required');
         return false;
-    }
-    if(username=='' || username==null){
+    } else if (user == '' || user == null) {
         alert('Username field is required');
         return false;
-    }
-    if(email=='' || email==null){
+    } else if (email == '' || email == null) {
         alert('Email field is required');
         return false;
-    }
-    if(npassword=='' || npassword==null){
+    } else if (npassword == '' || npassword == null) {
         alert('Password field is required');
         return false;
-    }
-    if(cpassword=='' || cpassword==null){
+    } else if (cpassword == '' || cpassword == null) {
         alert('Confirm Password field is required');
         return false;
+    } else if (npassword != cpassword) {
+        alert('Password do not match');
+        return false;
+    } else {
+        return true;
     }
 }
 
 //Login Validation
-function logvalid(){
-    let username = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
-    if(username== '' || username==null && password == '' || password==null){
+function logvalid() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    if ((username == '' || username == null) && (password == '' || password == null)) {
         alert('Both field are required');
         return false;
     }
-    if(username=='' || username==null){
+    if (username == '' || username == null) {
         alert('Username field is required');
         return false;
     }
-    if(password=="" || password==null){
+    if (password == "" || password == null) {
         alert('Password field is required');
         return false;
     }
-    if(username==password){
+    if (username == password) {
         alert('Both fields cannot be same');
         return false;
     }
 }
 
 //Contact Validation
-function conValid(){
+function conValid() {
     var firstName = document.getElementById('fname').value;
     var lastName = document.getElementById('lname').value;
-    var email =document.getElementById('mail').value;
+    var email = document.getElementById('mail').value;
     var msg = document.getElementById('msg').value;
 
-    if(firstName==''){
+    if (firstName == '') {
         alert('First name field is required');
         return false;
     }
 
-    if(lastName==''){
+    if (lastName == '') {
         alert('Last name field is required');
         return false;
     }
 
-    if(email==''){
+    if (email == '') {
         alert('Email field is required');
         return false;
     }
 
-    if(msg==''){
+    if (msg == '') {
         alert('Message field is required');
         return false;
     }
 }
-
-// function hidelogin() {
-//     document.getElementById('glass-card').style.display = 'none';
-// }
-
-// function showlogin() {
-//     document.getElementById('glass-card').style.display = 'block';
-// }
-
-// function myfunction() {
-    //  alert('Button CLicked')
-    // document.getElementById('lname').innerHTML = 'button is clicked.';
-    //document.getElementById('fname').style.color = 'red';
-    // document.getElementById('fname').style.display = 'none';
-//     var myname = document.getElementById('fname');
-//     if (myname.style.display == 'none') {
-//       myname.style.display = 'block';
-//     }
-
-//     else {
-//       myname.style.display = 'none';
-//     }
-// }
-
-// document.addEventListener('mouseup', function (e) {
-// var container = document.getElementById('inner-card')
-//     if (!container.contains(e.target)) {
-//       document.getElementById('glass-card').style.display = 'none';
-//     }
-// });
