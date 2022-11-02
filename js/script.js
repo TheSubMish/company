210f307c533f543554344cc36b0e1d665e830c80
TheSubMish <100989223+TheSubMish@users.noreply.github.com>
2022-11-02 17:43:34 +0545

    Update script.js

--- js/script.js
+++ js/script.js
@@ -108,27 +108,3 @@ function conValid() {
         return false;
     }
 }
-
-//  ADD DATA FROM FORM TO TABLE
-
-document.getElementById("submit").onclick = function() {
-    var table = document.getElementById("bill-table");
-    var row = table.insertRow(-1);
-    var sn = row.insertCell(0);
-    var item = row.insertCell(1);
-    var qty = row.insertCell(2);
-    var rate = row.insertCell(3);
-    var amt = row.insertCell(4);
-    var totAmt = document.getElementById("totAmt").innerHTML;
-
-    sn.innerHTML = 1;
-    item.innerHTML = document.getElementById("item").value;
-    qty.innerHTML = document.getElementById("quantity").value;
-    rate.innerHTML = document.getElementById("rate").value;
-    amt.innerHTML = qty.innerHTML * rate.innerHTML;
-
-    console.log(totAmt);
-    totAmt = parseInt(totAmt) + amt.innerHTML;
-    document.getElementById("totAmt").innerHTML = totAmt;
-    return false;
-}
